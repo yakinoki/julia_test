3ca751dc439ab36c6124daef8ad6a1980f727547
NAOYA\bundl <yakinoki>
2023-08-19 18:34:35 +0900

    add_code

---
+++ main.jl
@@ -0,0 +1,6 @@
+# モジュールを使用するためにインポート
+using .MyModule
+
+# モジュール内の関数を呼び出す
+result = my_function(5)
+println("結果: $result")
---
+++ mymodule.jl
@@ -0,0 +1,10 @@
+module MyModule
+
+export my_function
+
+# 関数の定義
+function my_function(x)
+    return x * 2
+end
+
+end
